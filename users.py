07bf0638004895f3ceeeca0d24c0b15b7115396f
omtusharkant <omtusharkant@gmail.com>
2024-05-29 07:52:00 +0530

    modified:   users.py
        added create user
        added modify user
        added deluser

--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+import pexpect
 import subprocess
 from datetime import datetime
 
@@ -5,7 +6,7 @@ class Users:
     def __init__(self):
         self.system_users_script = "./backend/system_users.sh"
         self.regular_users_script = "./backend/regular_users.sh"
-        
+        self.rootpasswd = ""
     def _run_command(self, command):
         try:
             result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
@@ -16,9 +17,15 @@ class Users:
 
     def _run_sudo_command(self,command):
         try:
-            result = subprocess.run(command, capture_output=True, text=True)
             
-            return result.stdout
+            # Start the passwd command
+            child = pexpect.spawn(f"sudo {command}")
+            child.expect("password for ")
+            if self.rootpasswd =="":
+                self.rootpasswd = input("enter root password")
+            child.sendline(self.rootpasswd) 
+            child.expect(pexpect.EOF)
+            return child.before.decode('utf-8')
         except subprocess.CalledProcessError as e:
             print(f"An error occurred while running the command: {e}")
             return [-1]
@@ -59,50 +66,64 @@ class Users:
 
         basic_info = self._run_command(f"getent passwd {user_name} ")[0].split(":")
         
-        Username = basic_info[0]
+        self.Username = basic_info[0]
 
         if basic_info[1] in ["x","*","!",""]:
             ispasswdprotected = "yes"
         else:
             ispasswdprotected = "no"
 
-        userID = basic_info[2]
+        self.userID = basic_info[2]
         
-        GroupID = basic_info[3]
+        self.GroupID = basic_info[3]
 
-        groups = self._run_command(f"groups {Username}")[0].split(":")[-1].strip().replace(" ",",")
+        self.pgroup = self._run_command(f"id -gn {user_name}")
 
-        fullName = basic_info[4]
+        self.groupsnames = self._run_command(f"groups {self.Username}")[0].split(":")[-1].strip().replace(" ",",")
 
-        homeDIR = basic_info[5]
+        self.fullName = basic_info[4]
 
-        shell = basic_info[6]
+        self.homeDIR = basic_info[5]
 
-        userType = "regular user"  if int(userID)>=1000 else "system user"
+        self.shell = basic_info[6]   #other shell types /usr/local/sbin\:/usr/local/bin\:/usr/sbin\:/usr/bin\:/sbin\:/bin\:/snap/bin,
 
-        sudoPermission = True if "sudo" in groups else False
 
+        userType = "regular user"  if int(self.userID)>=1000 else "system user"
+
+        self.sudoPermission = self._run_sudo_command(f" -l -U {self.Username}")
+        if "(ALL : ALL) ALL" in self.sudoPermission[-1]:
+            
+            self.sudoPermission = True
+        else:
+            self.sudoPermission = False
         if userType == "regular user":
-            # for backup beacuse it does not give year "grep 'useradd' /var/log/auth.log | grep '{Username}' |grep 'UID={userID}'"
-            userCreationdate = self._run_command(f"stat -c %w /home/{Username}")
+            
+
+            # for backup beacuse it does not give year "grep 'useradd' /var/log/auth.log | grep '{Username}' |grep 'UID={userID}'"     
+            #command 2  stat -c %w /home/marcus  it returns creation date of this home dir
+
+            userCreationdate = self._run_sudo_command(f"passwd -S {self.Username}").split(" ")[3]
             print(userCreationdate)
-            if -1 in userCreationdate:
+            if "new user:" in userCreationdate[0]:
+                print("new user")
+                date =  userCreationdate[0].split(":")
+                print(date)
+            elif "-1" in userCreationdate:
                 datetime_obj = ""
             elif userCreationdate:
-                # Timestamp string
-                timestamp_str = userCreationdate[0].replace(" ",",").split(",")
                 
-                newtimestamp = timestamp_str[0]+" "+timestamp_str[1].split(".")[0]
-                
-                # Format string to match the input timestamp%H:%M:%S.%f 
-                format_str = "%Y-%m-%d %H:%M:%S"
 
+                # Format string to match the input timestamp
+                format_str = "%m/%d/%Y"
+                output_format_str = "%Y-%m-%d"
                 # Parse the timestamp string into a datetime object
-                datetime_obj = datetime.strptime(newtimestamp, format_str)
+                datetime_obj = datetime.strptime(userCreationdate, format_str)
+                datetime_obj.strftime(output_format_str)
 
         else:
-            userCreationdate = self._run_sudo_command(["sudo","passwd", "-S", f"{Username}"]).split(" ")[2]
-
+            # this command return user creation date not time
+            userCreationdate = self._run_sudo_command(f"passwd -S {self.Username}")
+            print(userCreationdate)
             # Format string to match the input timestamp
             format_str = "%m/%d/%Y"
             output_format_str = "%Y-%m-%d"
@@ -111,28 +132,26 @@ class Users:
             datetime_obj.strftime(output_format_str)
 
 
-        last_login = self._run_command(f"last {Username}")
+        last_login = self._run_command(f"last {self.Username}")
         
         
 
         print("-------------------------------------")
-        print("User {Username} Detals")
+        print(f"User {self.Username} Detals")
         print("-------------------------------------")
-        print(f"Username ==: {Username}")
+        print(f"Username ==: {self.Username}")
         print(f"password protected ? : {ispasswdprotected}")
-        print(f"UserID : {(userID)}")
-        print(f"GroupID : {GroupID}")
-        print(f"Groups : {groups}")
-        print(f"FullName : {fullName}")
-        print(f"HomeDIR : {homeDIR}")
-        print(f"Shell : {shell}")
+        print(f"UserID : {(self.userID)}")
+        print(f"GroupID : {self.GroupID}")
+        print(f"Primary Group : {self.pgroup}")
+        print(f"Groups : {self.groupsnames}")
+        print(f"FullName : {self.fullName}")
+        print(f"HomeDIR : {self.homeDIR}")
+        print(f"Shell : {self.shell}")
         print(f"UserType : {userType}")
 
 
-        if sudoPermission == Username:
-            print("Sudo Privileges {True}")
-        else:
-            print("Sudo Privileges {False}")
+        print(f"sudo permission : {self.sudoPermission}")
 
         print(f"User Creation : {datetime_obj}")
         
@@ -146,7 +165,156 @@ class Users:
 
 
 
+    def createUser(self):
+        username = input("Enter user name: ")
+    
+        Bcommand = f"sudo useradd -m {username}"  # base command
         
+        choice = input("Do you want to add other values to save modification time (yes/no)? ").strip().lower()
+        
+        if choice == 'yes':
+            HDIR = input("Enter home directory path: ")
+            if HDIR:
+                Bcommand += f" -d {HDIR}"
+            
+            Expire = input("Enter expire date [format: MMDDhhmmyy] (default: 0): ").strip()
+            if Expire:
+                Bcommand += f" -e {Expire}"
+            
+            comment = input("Enter full name, phone number, organization or department (comma-separated): ").strip()
+            if comment:
+                Bcommand += f" -c '{comment}'"
+            
+            Primegroup = input("Enter primary group: ").strip()
+            if Primegroup:
+                Bcommand += f" -g {Primegroup}"
+            
+            SecondaryGroup = input("Enter secondary group names (comma-separated): ").strip()
+            if SecondaryGroup:
+                Bcommand += f" -G {SecondaryGroup}"
+            
+            skelDIR = input("Enter skeletal directory: ").strip()
+            if skelDIR:
+                Bcommand += f" -k {skelDIR}"
+            
+            
+            
+            shell = input("Enter user shell: ").strip()
+            if shell:
+                Bcommand += f" -s {shell}"
+            
+            uid = input("Enter UID: ").strip()
+            if uid:
+                Bcommand += f" -u {uid}"
+        
+        print(f"Generated command: {Bcommand}")
+        
+        self._run_command(Bcommand)
+
+        print(f"enter new password for {username}")
+        self._run_sudo_command(f"passwd {username}")
+
+    def modify_user(self,username):
+        ...
+        self.get_user_info(username)
+        modcommand = f"usermod {username}"
+        while True:
+                print("1. edit comment")
+                print("2. edit home dir")
+                print("3. edit primary group")
+                print("4. edit groups (comma separated)")
+                print("5. edit new username")
+                print("6. edit move content to new home dir")
+                print("7. edit shell")
+                print("8. edit UID")
+                print("9. edit password")
+                print("S.  show chnages")
+                print("0. exit")
+                choice = input("Input choice: ")
+
+                # Example of using the details from get_user_info
+                print(f"Current username: {self.Username}")
+                print(f"Selected choice: {choice}")
+                
+                if choice == '0':
+                    break
+            
+                # Implement modification based on choice
+                if choice == '1':
+                    print(f"prevous comment :{self.fullName}")
+                    new_comment = input("Enter new comment: ")
+                    #self._run_command(f"sudo usermod -c '{new_comment}' {self.Username}", shell=True)
+                    modcommand+= f" -c {new_comment}"
+                    
+                elif choice == '2':
+                    print(f"prevous home dir : {self.homeDIR}")
+                    new_home_dir = input("Enter new home directory: ")
+                    #self._run_command(f"sudo usermod -d {new_home_dir} {self.Username}", shell=True)
+                    modcommand+= f" -d {new_home_dir}"
+
+                elif choice == '3':
+                    print(f"prevous primary group : {self.pgroup}")
+                    new_primary_group = input("Enter new primary group: ")
+                    #self._run_command(f"sudo usermod -g {new_primary_group} {self.Username}", shell=True)
+                    modcommand+= f" -g {new_primary_group}"
+
+
+                elif choice == '4':
+                    print(f"prevous groups : {self.groupsnames}")
+                    new_groups = input("Enter new groups (comma separated): ")
+                    #self._run_command(f"sudo usermod -G {new_groups} {self.Username}", shell=True)
+                    modcommand+= f" -G {new_groups}"
+
+
+                elif choice == '5':
+                    print(f"prevous username : {self.Username}")
+                    new_username = input("Enter new username: ")
+                    #self._run_command(f"sudo usermod -l {new_username} {self.Username}", shell=True)
+                    modcommand+= f" -l {new_username}"
+
+
+                elif choice == '6':   #doubt check 
+                    print(f"prevous {self.fullName}")
+                    new_home_dir = input("Enter new home directory: ")
+                # self._run_command(f"sudo usermod -m -d {new_home_dir} {self.Username}", shell=True)
+                    #modcommand+=
+
+
+                elif choice == '7':
+                    print(f"prevous shell : {self.shell}")
+                    new_shell = input("Enter new shell: ")
+                    #self._run_command(f"sudo usermod -s {new_shell} {self.Username}", shell=True)
+                    modcommand+= f" -s {new_shell}"
+
+
+                elif choice == '8':
+                    print(f"prevous UID : {self.userID}")
+                    new_uid = input("Enter new UID: ")
+                    #self._run_command(f"sudo usermod -u {new_uid} {self.Username}", shell=True)
+                    modcommand+= f" -u {new_uid}"
+
+                elif choice == "S":
+                    print(modcommand)
+                else:
+                    print("Invalid choice")
+        print(modcommand)
+        self._run_sudo_command(modcommand)
+
+    def deleteuser(self,username):
+        ...
+        delcommand = f"deluser {username}"
+
+        rm_all_files = input("do you want to remove all file (yes/no)")
+        if rm_all_files == "yes":
+            delcommand+= " --remove-all-files"
+        rm_home = input("do you want to remove home (yes/no)")
+        if rm_home == "yes":
+            delcommand+= " --remove-home"
+
+        print(delcommand)
+        confirm = input(f"are you sure you want to delete {username}  (yes/no)")
+        if confirm == "yes":
+            self._run_sudo_command(delcommand)
 def main():
     users = Users()
     
@@ -154,7 +322,8 @@ def main():
     print("2. List regular users")
     print("3. get user info")
     print("4. create User")
-    
+    print("5. modify user")
+    print("6. delete user   (!with caution)")
     choice = input("Input: ")
 
     if choice == "1":
@@ -173,8 +342,13 @@ def main():
         users.get_user_info(input("Username : "))
     
     elif choice == "4":
+        users.createUser()
         
-        ...
+    elif choice == "5":
+        users.modify_user(input("enter username to edit"))
+
+    elif choice == "6":
+        users.deleteuser(input("enter username to delete"))
     else:
         print("Wrong choice")
 
